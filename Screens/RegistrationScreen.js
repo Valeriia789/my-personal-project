3ff3f7ef9dd85f8a2b3e346cd69dfb11896b5d10
Valeriia <valeriia.air@gmail.com>
2023-10-18 13:50:17 +0300

    розмітка RegistrationScreen

---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,60 @@
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, Text, TextInput, View } from "react-native";
+
+export const RegistrationScreen = () => {
+  return (
+    <View style={styles.formContainer}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.steps}>Step 1</Text>
+        <Text style={styles.stepTitle}>Enter your name</Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.steps}>Step 2</Text>
+        <Text style={styles.stepTitle}>Enter your email</Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.steps}>Step 3</Text>
+        <Text style={styles.stepTitle}>Create a safe password</Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  // height: Platform.OS === "ios" ? 50 : 100,
+  formContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputContainer: {
+    padding: 8,
+  },
+
+  steps: {
+    textAlign: "left",
+    fontSize: 14,
+  },
+
+  stepTitle: {
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+
+  input: {
+    width: 200,
+    padding: 6,
+    fontSize: 20,
+
+    backgroundColor: "#ff77",
+  },
+});
